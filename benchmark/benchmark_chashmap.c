4437e7df62f75f9e9857d53d4d037baf51c14aba
crt379 <chenrtang@gmail.com>
2024-10-02 23:27:51 +0800

    makefile add

--- benchmark/benchmark_chashmap.c
+++ benchmark/benchmark_chashmap.c
@@ -8,31 +8,24 @@ void benchmark_chashmap_set()
 {
     hashmap* map;
     clock_t start_t, end_t;
-    double total_t = 0;
     
     map = hashmap_new(sizeof(int), sizeof(int), 123456, NULL, NULL);
     start_t = clock();
     for (size i = 0; i < TEST_SIZE; i++) {
-        clock_t s = clock();
         hashmap_set(map, &i, &i);
-        clock_t e = clock();
-        total_t += (double)(e - s);
     }
     end_t = clock();
-    printf("set %i th time consuming: %fs, %fs\n", TEST_SIZE, total_t / CLOCKS_PER_SEC, (double)(end_t - start_t) / CLOCKS_PER_SEC);
+    printf("cap in %i set %i th time consuming: %fs\n", CAP, TEST_SIZE, (double)(end_t - start_t) / CLOCKS_PER_SEC);
     hashmap_free(map);
 
     map = hashmap_new_with_cap(CAP,sizeof(int), sizeof(int), 123456, NULL, NULL);
-    total_t = 0;
     start_t = clock();
     for (size i = 0; i < TEST_SIZE; i++) {
-        clock_t s = clock();
         hashmap_set(map, &i, &i);
-        clock_t e = clock();
-        total_t += (double)(e - s);
     }
     end_t = clock();
-    printf("cap in %i set %i th time consuming: %fs, %fs\n", CAP, TEST_SIZE, total_t / CLOCKS_PER_SEC, (double)(end_t - start_t) / CLOCKS_PER_SEC);
+    printf("cap in %i set %i th time consuming: %fs\n", CAP, TEST_SIZE, (double)(end_t - start_t) / CLOCKS_PER_SEC);
+    hashmap_free(map);
 }
 
 int main()
